7a038b53d6d22fe671fdfdf4e3502daf75abfb36
ejpje <emilypettit@emilysmbp12417.home>
2018-04-12 17:15:47 -0500

    Tileset and data
    Added link to mapbox tileset and added geojson data

--- js/main.js
+++ js/main.js
@@ -1,9 +1,33 @@
 /*Script by Emily Pettit, 2018*/
-var mymap = L.map("map").setView([51.505, -0.09], 13);
 
-L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
-    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery   <a href="http://mapbox.com">Mapbox</a>',
-    maxZoom: 18,
-    id: 'mapbox.streets',
-    accessToken: 'pk.eyJ1IjoiZWpwMyIsImEiOiJjamRrZ2g2d2EwMGoxMndxejdwd2poMGFhIn0.Ypo-SnygyDT2skpNIEQ60g'
-}).addTo(map);
+//function to instantiate the Leaflet map
+function createMap(){
+    //create the map
+    var map = L.map('map', {
+        center: [48, 20],
+        zoom: 4
+    });
+
+    //add OSM base tilelayer
+    L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v9/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiZWpwMyIsImEiOiJjamRrZ2g2d2EwMGoxMndxejdwd2poMGFhIn0.Ypo-SnygyDT2skpNIEQ60g', {
+        attribution: 'Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap contributors</a>'
+    }).addTo(map);
+
+    //call getData function
+    getData(map);
+};
+
+//function to retrieve the data and place it on the map
+function getData(map){
+    //load the data
+    $.ajax("data/raids.geojson", {
+        dataType: "json",
+        success: function(response){
+
+            //create a Leaflet GeoJSON layer and add it to the map
+            L.geoJson(response).addTo(map);
+        }
+    });
+};
+
+$(document).ready(createMap);
